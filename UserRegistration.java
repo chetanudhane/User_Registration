fe7aed363686dea9fe337f56193bab2d748e1385
chetan prakash udhane <78649010+chetanudhane@users.noreply.github.com>
2022-10-09 19:43:58 +0530

    UC3 Added

--- UserRegistration.java
+++ UserRegistration.java
@@ -7,16 +7,26 @@ public class UserRegistration {
 	public static void main(String[] args) {
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
+		String regex = "^[A-Z][a-z]{3,}$";
 		System.out.println("Enter First Name...");
 		String firstName = sc.next();
 		System.out.println("Enter Last Name...");
 		String lastName = sc.next();
+		System.out.println("Enter Mail Id...");
+		String emailId = sc.next();
 
 		boolean fName;
-		boolean lName;
+		boolean lName, email;
 
-		fName = firstName.matches("^[A-Z][a-z]{3,}$");
-		lName = lastName.matches("^[A-Z][a-z]{3,}$");
+		fName = firstName.matches(regex);
+		lName = lastName.matches(regex);
+		email = emailId.matches("^[A-Za-z0-9+_.-]+@(bl+.+co+.+)$");
+		if (email) {
+			System.out.println("Mail Id : " + emailId);
+
+		} else {
+			System.out.println("Invalid Type Of Name...");
+		}
 
 		if (fName && lName) {
 			System.out.println("Fisrt Name : " + firstName + "\nLast Name : " + lastName);
