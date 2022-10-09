1c14cf24e4253526c23d9808247711789b4fa935
chetan prakash udhane <78649010+chetanudhane@users.noreply.github.com>
2022-10-09 19:12:27 +0530

    UC1 Added

---
+++ UserRegistration.java
@@ -0,0 +1,23 @@
+package regex;
+
+import java.util.Scanner;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First Name...");
+		String fName = sc.next();
+
+		boolean result;
+		result = fName.matches("^[A-Z][a-z]{3,}$");
+
+		if (result) {
+			System.out.println("Fisrt Name Is Correct...");
+		} else {
+			System.out.println("Invalid Type Of Name...");
+		}
+
+	}
+}
